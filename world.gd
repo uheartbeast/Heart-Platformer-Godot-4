7144beb46a2621b3adc1e83f8d0f40ee29e52fe6
Heartbeast <heartbeast.studios@gmail.com>
2023-05-25 12:50:32 -0600

    Finished recording the Tilemap video

--- world.gd
+++ world.gd
@@ -1,12 +1,9 @@
 extends Node2D
 
-@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
-@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
 @onready var level_completed = $CanvasLayer/LevelCompleted
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.BLACK)
-	polygon_2d.polygon = collision_polygon_2d.polygon
 	Events.level_completed.connect(show_level_completed)
 
 func show_level_completed():
