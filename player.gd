966c2bc80cee3ffd4c7e36447ed5b51c7a1d7fdf
Heartbeast <heartbeast.studios@gmail.com>
2023-06-07 09:37:25 -0600

    P12 Wall Jump Fix

--- player.gd
+++ player.gd
@@ -5,10 +5,12 @@ extends CharacterBody2D
 var air_jump = false
 var just_wall_jumped = false
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var was_wall_normal = Vector2.ZERO
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var coyote_jump_timer = $CoyoteJumpTimer
 @onready var starting_position = global_position
+@onready var wall_jump_timer = $WallJumpTimer
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -21,19 +23,27 @@ func _physics_process(delta):
 	apply_air_resistance(input_axis, delta)
 	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
+	var was_on_wall = is_on_wall_only()
+	if was_on_wall:
+		was_wall_normal = get_wall_normal()
 	move_and_slide()
 	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
 	if just_left_ledge:
 		coyote_jump_timer.start()
 	just_wall_jumped = false
+	var just_left_wall = was_on_wall and not is_on_wall()
+	if just_left_wall:
+		wall_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * movement_data.gravity_scale * delta
 
 func handle_wall_jump():
-	if not is_on_wall_only(): return
-	var wall_normal = get_wall_normal()
+	if not is_on_wall_only() and wall_jump_timer.time_left <= 0.0: return
+	var wall_normal = get_wall_normal() 
+	if wall_jump_timer.time_left > 0.0:
+		wall_normal = was_wall_normal
 	if Input.is_action_just_pressed("jump"):
 		velocity.x = wall_normal.x * movement_data.speed
 		velocity.y = movement_data.jump_velocity
